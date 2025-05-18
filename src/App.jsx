8ce8965c661fa9bb8a896bea10dd5130d33c7df1
alfrzaa <egydiaalfariza74@gmail.com>
2025-05-18 20:58:06 +0700

    Menambahkan Page Awal + Test

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import PharmacyManagement from './page/test'
+import Dashboard from './page/dashboard'
+import StockManagement from './page/stok_obat'
+import laporan from './page/laporan'
+import ReportsSection from './page/laporan'
+
+function App() {
+  // return <div><PharmacyManagement /></div>
+  // return <div><Dashboard /></div>
+  // return <StockManagement />
+  return <ReportsSection />
+}
+
+export default App
