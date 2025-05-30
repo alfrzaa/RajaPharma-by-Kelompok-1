72d2ed349ac35225d517ce339d269dd21c87a42d
alfrzaa <egydiaalfariza74@gmail.com>
2025-05-30 12:21:17 +0700

    Menambahkan isi branch frontend-egydia ke branch frontend-baru

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import PharmacyManagement from './page/test'
+import Dashboard from './page/Dashboard-Admin'
+import StockManagement from './page/StockManagement-Admin'
+import LandingPage from './page/LandingPage'
+import LoginPage from './page/LoginPage'
+import SalesPage from './page/Sales-Kasir'
+import UserManagement from './page/UserManagement'
+// import Dashboard from './page/Dashboard-Kasir'
+import DashboardKasir from './page/Dashboard-Kasir'
+import ReportAdmin from './page/Report-Admin'
+
+function App() {
+  // return <Dashboard />
+  // return <DashboardKasir/>
+  // return <StockManagement />
+  // return <SalesPage />
+  // return <LandingPage />
+  return <LoginPage />
+  // return <ReportAdmin/>
+
+  // return <div><PharmacyManagement /></div>
+  // return <UserManagement/>
+}
+
+export default App
